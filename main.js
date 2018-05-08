f3947050bbf6fc8bc733b1320e2c9db1f4dce78b
swimjones <nwilliams17@punahou.edu>
2018-05-07 23:51:38 -0700

    updated email test + express-validator module

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const {app, BrowserWindow} = require('electron')
 let win
 
 function createWindow () {
-  win = new BrowserWindow({width: 800, height:600})
+  win = new BrowserWindow({width: 800, height:600, resizable: false})
 
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
